08d01529e0b2d6ac75a0b7f880f1d61d391786e7
Andrei <andrei.reiand@gmail.com>
2019-05-07 01:12:39 +0100

    Update

---
+++ CryptoTracker/Form1.Designer.cs
@@ -0,0 +1,165 @@
+﻿namespace CryptoTracker
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1CPol = new System.Windows.Forms.Button();
+            this.button2DPol = new System.Windows.Forms.Button();
+            this.textBox1AskPolPrice = new System.Windows.Forms.TextBox();
+            this.textBox1AskPolVol = new System.Windows.Forms.TextBox();
+            this.textBox1BidPolVol = new System.Windows.Forms.TextBox();
+            this.textBox1BidPolPrice = new System.Windows.Forms.TextBox();
+            this.groupBox1Pol = new System.Windows.Forms.GroupBox();
+            this.textBox1Channel = new System.Windows.Forms.TextBox();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.button3Update = new System.Windows.Forms.Button();
+            this.groupBox1Pol.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1CPol
+            // 
+            this.button1CPol.Location = new System.Drawing.Point(63, 79);
+            this.button1CPol.Name = "button1CPol";
+            this.button1CPol.Size = new System.Drawing.Size(75, 23);
+            this.button1CPol.TabIndex = 0;
+            this.button1CPol.Text = "Connect";
+            this.button1CPol.UseVisualStyleBackColor = true;
+            this.button1CPol.Click += new System.EventHandler(this.Button1CPol_Click);
+            // 
+            // button2DPol
+            // 
+            this.button2DPol.Location = new System.Drawing.Point(264, 79);
+            this.button2DPol.Name = "button2DPol";
+            this.button2DPol.Size = new System.Drawing.Size(75, 23);
+            this.button2DPol.TabIndex = 1;
+            this.button2DPol.Text = "Disconn";
+            this.button2DPol.UseVisualStyleBackColor = true;
+            this.button2DPol.Click += new System.EventHandler(this.Button2DPol_Click);
+            // 
+            // textBox1AskPolPrice
+            // 
+            this.textBox1AskPolPrice.Location = new System.Drawing.Point(63, 131);
+            this.textBox1AskPolPrice.Name = "textBox1AskPolPrice";
+            this.textBox1AskPolPrice.Size = new System.Drawing.Size(182, 22);
+            this.textBox1AskPolPrice.TabIndex = 2;
+            // 
+            // textBox1AskPolVol
+            // 
+            this.textBox1AskPolVol.Location = new System.Drawing.Point(264, 131);
+            this.textBox1AskPolVol.Name = "textBox1AskPolVol";
+            this.textBox1AskPolVol.Size = new System.Drawing.Size(182, 22);
+            this.textBox1AskPolVol.TabIndex = 3;
+            // 
+            // textBox1BidPolVol
+            // 
+            this.textBox1BidPolVol.Location = new System.Drawing.Point(264, 179);
+            this.textBox1BidPolVol.Name = "textBox1BidPolVol";
+            this.textBox1BidPolVol.Size = new System.Drawing.Size(182, 22);
+            this.textBox1BidPolVol.TabIndex = 4;
+            // 
+            // textBox1BidPolPrice
+            // 
+            this.textBox1BidPolPrice.Location = new System.Drawing.Point(63, 179);
+            this.textBox1BidPolPrice.Name = "textBox1BidPolPrice";
+            this.textBox1BidPolPrice.Size = new System.Drawing.Size(182, 22);
+            this.textBox1BidPolPrice.TabIndex = 5;
+            // 
+            // groupBox1Pol
+            // 
+            this.groupBox1Pol.Controls.Add(this.textBox1Channel);
+            this.groupBox1Pol.Controls.Add(this.richTextBox1);
+            this.groupBox1Pol.Controls.Add(this.button3Update);
+            this.groupBox1Pol.Location = new System.Drawing.Point(43, 50);
+            this.groupBox1Pol.Name = "groupBox1Pol";
+            this.groupBox1Pol.Size = new System.Drawing.Size(566, 1013);
+            this.groupBox1Pol.TabIndex = 6;
+            this.groupBox1Pol.TabStop = false;
+            this.groupBox1Pol.Text = "Poloniex";
+            // 
+            // textBox1Channel
+            // 
+            this.textBox1Channel.Location = new System.Drawing.Point(423, 80);
+            this.textBox1Channel.Name = "textBox1Channel";
+            this.textBox1Channel.Size = new System.Drawing.Size(124, 22);
+            this.textBox1Channel.TabIndex = 9;
+            this.textBox1Channel.Text = "USDT_BTC";
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(20, 177);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(527, 818);
+            this.richTextBox1.TabIndex = 8;
+            this.richTextBox1.Text = "";
+            // 
+            // button3Update
+            // 
+            this.button3Update.Location = new System.Drawing.Point(328, 29);
+            this.button3Update.Name = "button3Update";
+            this.button3Update.Size = new System.Drawing.Size(75, 23);
+            this.button3Update.TabIndex = 7;
+            this.button3Update.Text = "Update";
+            this.button3Update.UseVisualStyleBackColor = true;
+            this.button3Update.Click += new System.EventHandler(this.Button3Update_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1596, 1118);
+            this.Controls.Add(this.textBox1BidPolPrice);
+            this.Controls.Add(this.textBox1BidPolVol);
+            this.Controls.Add(this.textBox1AskPolVol);
+            this.Controls.Add(this.textBox1AskPolPrice);
+            this.Controls.Add(this.button2DPol);
+            this.Controls.Add(this.button1CPol);
+            this.Controls.Add(this.groupBox1Pol);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1Pol.ResumeLayout(false);
+            this.groupBox1Pol.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1CPol;
+        private System.Windows.Forms.Button button2DPol;
+        private System.Windows.Forms.TextBox textBox1AskPolPrice;
+        private System.Windows.Forms.TextBox textBox1AskPolVol;
+        private System.Windows.Forms.TextBox textBox1BidPolVol;
+        private System.Windows.Forms.TextBox textBox1BidPolPrice;
+        private System.Windows.Forms.GroupBox groupBox1Pol;
+        private System.Windows.Forms.Button button3Update;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.TextBox textBox1Channel;
+    }
+}
+
---
+++ CryptoTracker/Form1.cs
@@ -0,0 +1,210 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using WebSocketSharp;
+
+namespace CryptoTracker
+{
+    public partial class Form1 : Form
+    {
+        
+        PoloniexWS wsPol; 
+        public Form1()
+        {
+            InitializeComponent();
+
+            wsPol = new PoloniexWS(textBox1Channel.Text);
+            wsPol.ws.OnMessage += (sender, e) => UpdatePolTextBoxes();
+        }
+        
+
+        private void Button1CPol_Click(object sender, EventArgs e)
+        {
+            wsPol.Connect();
+            button2DPol.Enabled = true;
+            button1CPol.Enabled = false;
+        }
+
+        private void Button2DPol_Click(object sender, EventArgs e)
+        {
+            wsPol.Disconn();
+            button2DPol.Enabled = false;
+            button1CPol.Enabled = true;
+        }
+
+        private void Button3Update_Click(object sender, EventArgs e)
+        {
+            UpdatePolTextBoxes();
+        }
+
+        private void UpdatePolTextBoxes()
+        {
+            double price, vol;
+            (price, vol) = wsPol.GetHighestBid();
+            if(this.InvokeRequired) this.Invoke(new Action(() => textBox1BidPolPrice.Text = price.ToString()));
+            if (this.InvokeRequired) this.Invoke(new Action(() => textBox1BidPolVol.Text = vol.ToString()));
+
+            (price, vol) = wsPol.GetLowestAsk();
+            if (this.InvokeRequired) this.Invoke(new Action(() => textBox1AskPolPrice.Text = price.ToString()));
+            if (this.InvokeRequired) this.Invoke(new Action(() => textBox1AskPolVol.Text = vol.ToString()));
+
+            if (this.InvokeRequired) this.Invoke(new Action(() => richTextBox1.AppendText(wsPol.GetSpreadInText())));
+        }
+    }
+
+    public class PoloniexWS : BaseWS
+    {
+        string path = @"c:\temp\_Poloniex_Tokens.txt";
+        static SortedDictionary<double, double> bidLadder;
+        static SortedDictionary<double, double> askLadder;
+        string channel;
+        public PoloniexWS(string chn) : base("wss://api2.poloniex.com")
+        {
+            channel = "{\"command\": \"subscribe\", \"channel\": \"" + chn + "\"}";
+            bidLadder = new SortedDictionary<double, double>();
+            askLadder = new SortedDictionary<double, double>();
+            ws.OnMessage += (sender, e) =>
+            {
+                //using (StreamWriter sw = File.AppendText(path)) {sw.WriteLine(e.Data);}
+                UpdateLadders(e.Data);
+            };
+        }
+
+        private static double highestBid = 0, lowestAsk = 0;
+        public (double,double) GetHighestBid()
+        {
+            lock (bidLadder)
+            {
+                if (bidLadder.Count != 0) while (bidLadder[bidLadder.Keys.Max()] == 0) bidLadder.Remove(bidLadder.Keys.Max());
+                if (bidLadder.Count != 0)
+                {
+                    highestBid = bidLadder.Keys.Max();
+                    return (highestBid, bidLadder[highestBid]);
+                }
+                else
+                {
+                    highestBid = 0;
+                    return (0, 0);
+                }
+            }
+        }
+        public (double, double) GetLowestAsk()
+        {
+            lock (askLadder)
+            {
+                if (askLadder.Count != 0) while (askLadder[askLadder.Keys.Min()] == 0) askLadder.Remove(askLadder.Keys.Min());
+                if (askLadder.Count != 0)
+                {
+                    lowestAsk = askLadder.Keys.Min();
+                    return (lowestAsk, askLadder[lowestAsk]);
+                }
+                else
+                {
+                    lowestAsk = 0;
+                    return (0, 0);
+                }
+            }
+        }
+        public double GetSpread()
+        {
+            return lowestAsk - highestBid;
+        }
+        public string GetSpreadInText()
+        {
+            string str = "Spread: " + (lowestAsk - highestBid).ToString() + "  =  " + lowestAsk + " - " + highestBid + "\n";
+            return str; 
+        }
+        private void UpdateLadders(string data)
+        {
+            (List<string> bidTokensList, List<string> askTokensList) = GetUpdateTokens(data);
+            if (askTokensList.Count != 0) UpdateLadder(askTokensList, 0);
+            if (bidTokensList.Count != 0) UpdateLadder(bidTokensList, 1);
+        }
+
+        private void UpdateLadder(List<string> tokensList, int v)
+        {
+            foreach (string token in tokensList)
+            {
+                //using (StreamWriter sw = File.AppendText(path)) {sw.WriteLine("v: " + v + "; "  +token);}
+                UpdateLadder2(token, v == 0 ? askLadder : v == 1 ? bidLadder : throw new System.ArgumentException("UpdateLadder2: Parameter sortedDict cannot be null", "sortedDict"));
+            }
+        }
+
+        private void UpdateLadder2(string token, SortedDictionary<double, double> sortedDict)
+        {
+            string[] tokens = token.Split(','); // indexes: 0 = "o", 1 = "0 or 1", 2 = Price, 3 = Vol/Qty
+            double price = Double.Parse(tokens[2].Trim('"'));
+            double volum = Double.Parse(tokens[3].Trim('"'));
+            if (volum == 0 && sortedDict.ContainsKey(price))
+                sortedDict.Remove(price);   // price level has no more bid/ask participant; we can delete the entry from the dict
+            else
+                sortedDict[price] = volum;  // update of price level
+        }
+
+        private (List<string>, List<string>) GetUpdateTokens(string data)
+        {
+            List<string> bidTokensList = new List<string>();
+            List<string> askTokensList = new List<string>();
+
+            string worker = data;
+            while(worker != "")
+            {
+                string token;
+                (worker, token) = GetNextToken(worker);
+                token = token.Trim(new char[] { '[', ']' }).Replace(" ", string.Empty);
+                if (token.Contains("\"o\",0")) // we have an ask update
+                {
+                    askTokensList.Add(token);
+                }
+                else if (token.Contains("\"o\",1")) // we have a bid update 
+                {
+                    bidTokensList.Add(token);
+                }
+            }
+            return (bidTokensList, askTokensList);
+        }
+
+        private (string, string) GetNextToken(string worker)
+        {
+            int lastIndexOf = worker.Substring(0, worker.IndexOf(']')).LastIndexOf('[');
+            string token = worker.Substring(lastIndexOf, worker.IndexOf(']') - lastIndexOf + 1);
+            string leftOver = RemainingString(worker, lastIndexOf, worker.IndexOf(']') - lastIndexOf + 1);
+            return (leftOver, token);
+        }
+        public string RemainingString(string str, int start, int length)
+        {
+            return Regex.Replace(str, "^(.{" + start + "})(?:.{" + length + "})(.*)$", "$1$2");
+        }
+        public void Connect()
+        {
+            ws.Connect();
+            if (ws.IsAlive) ws.Send(channel);
+            else throw new System.ArgumentException("PoloniexWS:Connect - conn failure!");
+        }
+        public void Disconn()
+        {
+            ws.Close();
+        }
+    }
+
+    public class BaseWS
+    {
+        String urlStr;
+        private enum SslProtocolsHack { Tls = 192, Tls11 = 768, Tls12 = 3072 }
+        public WebSocket ws; 
+        public BaseWS(string urlStr)
+        {
+            this.urlStr = urlStr;
+            ws = new WebSocket(urlStr);
+            ws.SslConfiguration.EnabledSslProtocols = (System.Security.Authentication.SslProtocols)(SslProtocolsHack.Tls12 | SslProtocolsHack.Tls11 | SslProtocolsHack.Tls);
+        }
+    }
+}
